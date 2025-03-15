616293922272c3d1b7f05034b0d386de1a9e7f65
fortisvariano <vf41922@mail.harpercollege.edu>
2025-03-14 19:55:28 -0500

    Seperated interfaces and implementations.

--- Parser.h
+++ Parser.h
@@ -25,29 +25,9 @@ private:
         return postfix.front().getType() == UNARY_OPERATOR;
     }
 
-    VariableNode* newVariableNode(const Token& var) {
-        for(size_t i = 0; i < variables.size(); ++i) {
-            if(var.getValue() == variables[i]->getName()) {
-                return variables[i];
-            }
-        }
-        variables.push_back(new VariableNode(postfix.front().getValue()));
-        return variables.back();
-    }
-    BinaryOperatorNode* newBinaryOperatorNode(const Token& bop) {
-        BinaryOperatorNode* newNode = new BinaryOperatorNode(bop.getValue());
-        newNode->setR(stack.back());
-        stack.pop_back();
-        newNode->setL(stack.back());
-        stack.pop_back();
-        return newNode;
-    }
-    UnaryOperatorNode* newUnaryOperatorNode(const Token& bop) {
-        UnaryOperatorNode* newNode = new UnaryOperatorNode(bop.getValue());
-        newNode->set(stack.back());
-        stack.pop_back();
-        return newNode;
-    }
+    VariableNode* newVariableNode(const Token& var);
+    BinaryOperatorNode* newBinaryOperatorNode(const Token& bop);
+    UnaryOperatorNode* newUnaryOperatorNode(const Token& bop);
 public:
     Parser(const Deque<Token>& Postfix) : postfix(Postfix), stack(), variables() {}
     void reset(const Deque<Token>& Postfix) {
@@ -55,22 +35,7 @@ public:
         stack = Deque<ExpressionNode*>();
         variables = std::vector<VariableNode*>();
     }
-    void parse() {
-        while(!postfix.empty()) {
-            ExpressionNode* newNode;
-            if(numNext()) {
-                newNode = new ConstantNode(std::stod(postfix.front().getValue()));
-            } else if(varNext()) {
-                newNode = newVariableNode(postfix.front());
-            } else if(bopNext()) {
-                newNode = newBinaryOperatorNode(postfix.front());
-            } else if(uopNext()) {
-                newNode = newUnaryOperatorNode(postfix.front());
-            }
-            stack.push_back(newNode);
-            postfix.pop_front();
-        }
-    }
+    void parse();
     ExpressionNode* getExpressionTree() {
         return stack.front();
     }
--- PostFix.h
+++ PostFix.h
@@ -33,56 +33,11 @@ private:
                infix.front().getType() == UNARY_OPERATOR;
     }
 
-    bool shouldPopForOp(Token op) {
-        short precedenceOp = precedence(op);
-        short precedenceSt = precedence(opStack.back());
-        if(op.getType() == UNARY_OPERATOR) {
-            return false;
-        }
-        if(precedenceSt < precedenceOp) {
-            return false;
-        } else if(precedenceSt == precedenceOp) {
-            return associativity(op) == LEFT;
-        } else {
-            return true;
-        }
-    }
+    bool shouldPopForOp(Token op);
 
-    Associativity associativity(Token op) {
-        if(op.getValue() == "+") {
-            return LEFT;
-        } else if(op.getValue()== "-") {
-            if(op.getType() == BINARY_OPERATOR) {
-                return LEFT;
-            } else {
-                return RIGHT;
-            }
-        } else if(op.getValue() == "*") {
-            return LEFT;
-        } else if(op.getValue() == "/") {
-            return LEFT;
-        } else if(op.getValue() == "%") {
-            return LEFT;
-        } else if(op.getValue() == "^") {
-            return RIGHT;
-        }
-    }
+    Associativity associativity(Token op);
 
-    short precedence(Token op) {
-        if(op.getValue() == "+") {
-            return 1;
-        } else if(op.getValue()== "-") {
-            return 1;
-        } else if(op.getValue() == "*") {
-            return 2;
-        } else if(op.getValue() == "/") {
-            return 2;
-        } else if(op.getValue() == "%") {
-            return 2;
-        } else if(op.getValue() == "^") {
-            return 3;
-        }
-    }
+    short precedence(Token op);
 public:
     PostFix(const Deque<Token>& Infix) : infix(Infix), opStack(), postfixTokens() {}
     void reset(const Deque<Token>& Infix) {
@@ -90,42 +45,7 @@ public:
         opStack = Deque<Token>();
         postfixTokens = Deque<Token>();
     }
-    void postfix() {
-        while(!infix.empty()) {
-            if(numNext()) {
-                postfixTokens.push_back(infix.front());
-                infix.pop_front();
-            } else if(varNext()) {
-                postfixTokens.push_back(infix.front());
-                infix.pop_front();
-            } else if(oparNext()) {
-                opStack.push_back(infix.front());
-                infix.pop_front();
-            } else if(cparNext()) {
-                while(!opStack.empty() && opStack.back().getType() != PARENTHESES) {
-                    postfixTokens.push_back(opStack.back());
-                    opStack.pop_back();
-                }
-                if(infix.front().getType() == PARENTHESES) {
-                    opStack.pop_back();
-                }
-                infix.pop_front();
-            } else if(opNext()) {
-                while(!opStack.empty() && shouldPopForOp(infix.front())) {
-                    postfixTokens.push_back(opStack.back());
-                    opStack.pop_back();
-                }
-                opStack.push_back(infix.front());
-                infix.pop_front();
-            }
-        }
-        while(!opStack.empty()) {
-            if(opStack.back().getType() != PARENTHESES) {
-                postfixTokens.push_back(opStack.back());
-            }
-            opStack.pop_back();
-        }
-    }
+    void postfix();
     Deque<Token> getPostfixTokens() const {
         return postfixTokens;
     }
